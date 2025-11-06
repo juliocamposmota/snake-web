21072d93802e2e77ef23cb2bfc9d81120ce9941d
julio-findme <julio@findme.id>
2025-11-05 21:26:52 -0300

    feat: add drawSnake method

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,20 @@
 const ctx = document.getElementById('canvas').getContext('2d');
 
-canvas.style.backgroundColor = 'lightblue';
+const size = 30;
+const defaultPosition = { x: 270, y: 270 };
+
+let snake = [defaultPosition];
+
+const drawSnake = () => {
+  ctx.fillStyle = "#999999";
+  
+  for (let i = 0; i < snake.length; i++) {
+    if (i === snake.length - 1) {
+      ctx.fillStyle = "#d1d1d1";
+    }
+
+    ctx.fillRect(snake[i].x, snake[i].y, size, size);
+  }
+}
+
+drawSnake();
