526ccbbd0d1ebdad1d7e85e170576e4ce3b2d119
julio-findme <julio@findme.id>
2025-11-05 21:30:31 -0300

    feat: add moveSnake method

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 const ctx = document.getElementById('canvas').getContext('2d');
 
 const size = 30;
+const direction = 'RIGHT';
 const defaultPosition = { x: 270, y: 270 };
 
 let snake = [defaultPosition];
@@ -17,4 +18,30 @@ const drawSnake = () => {
   }
 }
 
-drawSnake();
+const moveSnake = (direction) => {
+  const head = { ...snake[snake.length - 1] };
+
+  switch (direction) {
+    case 'UP':
+      head.y -= size;
+      break;
+    case 'DOWN':
+      head.y += size;
+      break;
+    case 'LEFT':
+      head.x -= size;
+      break;
+    case 'RIGHT':
+      head.x += size;
+      break;
+  }
+
+  snake.push(head);
+  snake.shift();
+}
+
+setInterval(() => {
+  ctx.clearRect(0, 0, 600, 600);
+  moveSnake(direction);
+  drawSnake();
+}, 100);
